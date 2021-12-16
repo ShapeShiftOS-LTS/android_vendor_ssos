f813bdf7476a19b4183b8cd338022c4c71d6dbb7
LeddaZ <leonardoledda@gmail.com>
2021-12-16 17:04:57 +0100

    ssos: 2.9 LTS

--- config/branding.mk
+++ config/branding.mk
@@ -1,25 +1,18 @@
 BUILD_DATE := $(shell date +%Y%m%d)
 BUILD_TIME := $(shell date +%H%M)
 # Versioning System
-# Use signing keys for only official
-ifndef SSOS_BUILD_TYPE
-    SSOS_BUILD_TYPE := UNOFFICIAL
-endif
+SSOS_BUILD_TYPE := LTS
 
 # Set all versions
-SSOS_BASE_VERSION := 2.8
-SSOS_CODENAME := Duosion
+SSOS_BASE_VERSION := 2.9
+SSOS_CODENAME := Glaceon
 SSOS_PLATFORM_VERSION := 11
 TARGET_PRODUCT_SHORT := $(subst ssos_,,$(TARGET_PRODUCT))
 SSOS_VERSION := ShapeShiftOS-$(SSOS_BASE_VERSION)-$(SSOS_CODENAME)-$(SSOS_BUILD_TYPE)-$(TARGET_PRODUCT_SHORT)-$(BUILD_DATE)-$(BUILD_TIME)
 ROM_FINGERPRINT := ShapeShiftOS/$(PLATFORM_VERSION)/$(SSOS_BUILD_TYPE)/$(BUILD_DATE)$(BUILD_TIME)
 DISPLAY_SSOS_SETTINGS := $(SSOS_BASE_VERSION) \\ $(SSOS_CODENAME) \\ $(SSOS_BUILD_TYPE)
 
-ifeq ($(SSOS_BUILD_TYPE),OFFICIAL)
-    PLATFORM_SSOS_VERSION := $(SSOS_BASE_VERSION).REL.$(shell date +%m%d%H%M)
-else
-    PLATFORM_SSOS_VERSION := $(SSOS_BASE_VERSION).INT.$(shell date +%m%d%H%M)
-endif
+PLATFORM_SSOS_VERSION := $(SSOS_BASE_VERSION).LTS.$(shell date +%m%d%H%M)
 
 # Declare it's a ShapeShiftOS build
 SSOS_BUILD := true
